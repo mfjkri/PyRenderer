6c6755bc47caaa8988f378823124083073a57774
mfjkri <muhammadfikri6844@gmail.com>
2022-05-27 15:31:01 +0800

    Added transformation methods for Models

--- src/Model.py
+++ src/Model.py
@@ -1,16 +1,22 @@
 from typing import Tuple
 
+import numpy
+import Transform
+
 
 class Model():
-    def __init__(self, path: str = None, vertices: list = None, faces: list = None):
+    def __init__(self, scene, path: str = None, vertices: list = None, faces: list = None):
         if path:
             vertices, faces = self.load_from_file(path)
         else:
             assert (not vertices and not faces) or (
                 vertices or faces), "BOTH VERTICES AND FACES MUST BE PROVIDED"
 
-        self.vertices = vertices
-        self.faces = faces
+        self.scene = scene
+        self.vertices: numpy.ndarray = numpy.array(
+            [numpy.array(v) for v in vertices])
+        self.faces: numpy.ndarray = numpy.array(
+            [numpy.array(face) for face in faces])
 
     def load_from_file(self, path: str) -> Tuple:
         vertices, faces = [], []
@@ -31,3 +37,18 @@ class Model():
                     )
 
         return vertices, faces
+
+    def translate(self, pos):
+        self.vertices = self.vertices @ Transform.translate(pos)
+
+    def scale(self, scale_to):
+        self.vertices = self.vertices @ Transform.scale(scale_to)
+
+    def rotate_x(self, angle):
+        self.vertices = self.vertices @ Transform.rotate_x(angle)
+
+    def rotate_y(self, angle):
+        self.vertices = self.vertices @ Transform.rotate_y(angle)
+
+    def rotate_z(self, angle):
+        self.vertices = self.vertices @ Transform.rotate_z(angle)
