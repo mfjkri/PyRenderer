4efac6043975bf5f9620af27208e13db5d9a56ee
mfjkri <muhammadfikri6844@gmail.com>
2022-05-27 15:27:51 +0800

    Added rotations to transformations

--- src/Transform.py
+++ src/Transform.py
@@ -1,3 +1,4 @@
+import math
 import numpy
 
 
@@ -15,5 +16,32 @@ def scale(n):
         [n, 0, 0, 0],
         [0, n, 0, 0],
         [0, 0, n, 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def rotate_x(a):
+    return numpy.array([
+        [1, 0, 0, 0],
+        [0, math.cos(a), math.sin(a), 0],
+        [0, -math.sin(a), math.cos(a), 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def rotate_y(a):
+    return numpy.array([
+        [math.cos(a), 0, -math.sin(a), 0],
+        [0, 1, 0, 0],
+        [math.sin(a), 0, math.cos(a), 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def rotate_z(a):
+    return numpy.array([
+        [math.cos(a), math.sin(a), 0, 0],
+        [-math.sin(a), math.cos(a), 0, 0],
+        [0, 0, 1, 0],
         [0, 0, 0, 1]
     ])
